bc28d6968bea0a55a62b91f087ec8b82199e6d6c
Cale <cale.short@gmail.com>
2017-03-02 12:28:45 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='liveengage_data_app',
+      version='0.1',
+      description='Unofficial wrapper for LiveEngage data APIs',
+      url='https://github.com/LiveEngage-Examples/liveengage_data_app',
+      author='WildYorkies',
+      author_email='',
+      license='MIT',
+      packages=['liveengage_data_app'],
+      zip_safe=False)
