a6fb8398936cbcb274271030133e4a0cc59cf3ff
Cale <cale.short@gmail.com>
2017-03-17 12:27:39 -0400

    fix setup

--- setup.py
+++ setup.py
@@ -16,4 +16,22 @@ setup(name='liveengage_data_app',
           'requests',
           'requests_oauthlib',
       ],
+      classifiers=[
+          # How mature is this project? Common values are
+          #   3 - Alpha
+          #   4 - Beta
+          #   5 - Production/Stable
+          'Development Status :: 3 - Alpha',
+
+          # Indicate who your project is intended for
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: API Wrapper',
+
+          # Pick your license as you wish (should match "license" above)
+          'License :: OSI Approved :: MIT License',
+
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
+          'Programming Language :: Python :: 3.5',
+      ],
       zip_safe=False)
